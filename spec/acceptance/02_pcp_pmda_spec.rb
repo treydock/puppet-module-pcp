27e62aae67db8be39c0a2a89dff1b68222404142
Trey Dockendorf <tdockendorf@osc.edu>
2018-12-04 10:20:48 -0500

    Support setting pmda arguments

--- spec/acceptance/02_pcp_pmda_spec.rb
+++ spec/acceptance/02_pcp_pmda_spec.rb
@@ -108,4 +108,59 @@ describe 'pcp::pmda define:' do
       its(:exit_status) { should_not eq 0 }
     end
   end
+
+  context 'install proc pmda with args' do
+    it 'should run successfully' do
+      pp =<<-EOS
+        pcp::pmda { 'proc':
+          ensure      => 'present',
+          has_package => false,
+          args        => '-A',
+        }
+      EOS
+
+      apply_manifest(pp, :catch_failures => true)
+      sleep(15)
+      apply_manifest(pp, :catch_changes => true)
+    end
+
+    describe file('/var/lib/pcp/pmdas/proc/Install') do
+      its(:content) { should match /^args="-A"$/ }
+    end
+
+    describe file('/etc/pcp/pmcd/pmcd.conf') do
+      its(:content) { should match /^proc.*\-A$/ }
+    end
+
+    describe command('pminfo proc') do
+      its(:exit_status) { should eq 0 }
+    end
+  end
+
+  context 'remove proc pmda args' do
+    it 'should run successfully' do
+      pp =<<-EOS
+        pcp::pmda { 'proc':
+          ensure      => 'present',
+          has_package => false,
+        }
+      EOS
+
+      apply_manifest(pp, :catch_failures => true)
+      sleep(15)
+      apply_manifest(pp, :catch_changes => true)
+    end
+
+    describe file('/var/lib/pcp/pmdas/proc/Install') do
+      its(:content) { should_not match /^args.*/ }
+    end
+
+    describe file('/etc/pcp/pmcd/pmcd.conf') do
+      its(:content) { should match /^proc.*pmdaproc \-d 3(\s+)?$/ }
+    end
+
+    describe command('pminfo proc') do
+      its(:exit_status) { should eq 0 }
+    end
+  end
 end
