b9b6e7d15128625d6c01b2af7482a43509d52977
Trey Dockendorf <tdockendorf@osc.edu>
2019-05-07 14:33:58 -0400

    Remove repo when ensure is absent

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -3,11 +3,15 @@ class pcp::repo {
   if $pcp::manage_repo {
     case $::osfamily {
       'RedHat': {
-        yumrepo { 'pcp':
-          descr    => 'pcp',
-          baseurl  => $pcp::repo_baseurl,
-          enabled  => '1',
-          gpgcheck => '0',
+        if $pcp::ensure == 'absent' {
+          file { '/etc/yum.repos.d/pcp.repo': ensure => 'absent' }
+        } else {
+          yumrepo { 'pcp':
+            descr    => 'pcp',
+            baseurl  => $pcp::repo_baseurl,
+            enabled  => '1',
+            gpgcheck => '0',
+          }
         }
       }
       default: {
