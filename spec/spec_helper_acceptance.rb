cfdc96b1929839786a645d1dea118d6183cbaa8d
Trey Dockendorf <tdockendorf@osc.edu>
2019-05-09 10:14:23 -0400

    Pdk sync

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -5,6 +5,7 @@ require 'beaker/module_install_helper'
 
 dir = File.expand_path(File.dirname(__FILE__))
 Dir["#{dir}/acceptance/shared_examples/**/*.rb"].sort.each { |f| require f }
+require 'spec_helper_acceptance_local' if File.file?(File.join(File.dirname(__FILE__), 'spec_helper_acceptance_local.rb'))
 
 run_puppet_install_helper
 install_module_on(hosts)
