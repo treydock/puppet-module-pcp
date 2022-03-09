52e4809c77ecf63ef65567a3751c31490a5f6f00
treydock <treydock@gmail.com>
2022-03-09 16:26:07 -0500

    Update spec/shared_examples/pcp_resources.rb

--- spec/shared_examples/pcp_resources.rb
+++ spec/shared_examples/pcp_resources.rb
@@ -27,10 +27,10 @@ shared_examples_for 'pcp::resources' do |_facts|
     it { is_expected.to have_pcp__pmda_resource_count(1) }
     it { is_expected.to contain_pcp__pmda('test').with_has_package('true') }
 
-    it { is_expected.to have_pcp__pmie_resource_count(1) }
+    it { is_expected.to have_pcp__pmie_resource_count(2) }
     it { is_expected.to contain_pcp__pmie('test').with_log_file('EXAMPLE') }
 
-    it { is_expected.to have_pcp__pmlogger_resource_count(1) }
+    it { is_expected.to have_pcp__pmlogger_resource_count(2) }
     it { is_expected.to contain_pcp__pmlogger('test').with_log_dir('EXAMPLE') }
   end
 end
